16fc992e5da7577c29bb30b0b4b7cf44fcf40acc
ms06s <uselesspassion@outlook.com>
2020-07-31 21:31:02 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,15 @@ import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
+  {
+    path: '*',
+    component:  NotFound
+  },
   {
     path: '/',
     redirect: '/money'
