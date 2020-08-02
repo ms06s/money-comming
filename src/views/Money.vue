90a94c595c4a45bc8b3df7d98d33dfcd1694df7b
ms06s <uselesspassion@outlook.com>
2020-08-02 15:17:26 +0800

    组件 Tags.vue 完成

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,20 +3,25 @@
     <NumberPad></NumberPad>
     <Types></Types>
     <Notes></Notes>
-    <Tags></Tags>
+    <Tags :data-source.sync="tags"></Tags>
   </Layout>
 </template>
 
-<script lang="ts">
-import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
-import Notes from '@/components/Money/Notes.vue';
-import Tags from '@/components/Money/Tags.vue';
+<script>
+import NumberPad from '@/components/Money/NumberPad.vue'
+import Types from '@/components/Money/Types.vue'
+import Notes from '@/components/Money/Notes.vue'
+import Tags from '@/components/Money/Tags.vue'
 
 export default {
   name: 'Money',
   components: {Tags, Notes, Types, NumberPad},
-};
+  data() {
+    return {
+      tags: ['衣', '食', '住', '行']
+    }
+  }
+}
 </script>
 
 <style lang="scss">
