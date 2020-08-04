77501a0816f9042471c5ebe9301930581df09781
ms06s <uselesspassion@outlook.com>
2020-08-04 10:02:55 +0800

    封装 record 操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,10 +17,9 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
+import {Component} from 'vue-property-decorator';
 
-const recordList = recordListModel.fetch();
+
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad}
@@ -30,7 +29,7 @@ export default class Money extends Vue {
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
-  recordList: RecordItem[] = recordList;
+  recordList = window.recordList;
 
 
   onUpdateAmount(value: string) {
@@ -46,13 +45,9 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record)
+    window.createRecord(this.record)
   }
 
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
-  }
 }
 </script>
 
